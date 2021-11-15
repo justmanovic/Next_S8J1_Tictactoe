8c2fb7fd5d0316d6f7683c53ef2c162238d17fb4
jonathan justman <jojo.justman@gmail.com>
2021-11-15 18:08:24 +0100

    3 levels of IA OK

--- morpion.js
+++ morpion.js
@@ -1,46 +1,63 @@
 class Morpion {
-	humanPlayer = 'J1';
-	iaPlayer = 'J2';
+    humanPlayer = "J1";
+    iaPlayer = "J2";
+    iaLevel = 2;
     turn = 0;
-	gameOver = false;
+    gameOver = false;
 
-	gridMap = [
-		[null, null, null],
-		[null, null, null],
-		[null, null, null],
-	];
+    gridMap = [
+        [null, null, null],
+        [null, null, null],
+        [null, null, null],
+    ];
 
-	constructor(firstPlayer = 'J1') {
-		this.humanPlayer = firstPlayer;
-		this.iaPlayer = (firstPlayer === 'J1') ? 'J2' : 'J1';
-		this.initGame();
-	}
+    coordinates = [
+        [
+            [0, 0],
+            [0, 1],
+            [0, 2],
+        ],
+        [
+            [1, 0],
+            [1, 1],
+            [1, 2],
+        ],
+        [
+            [2, 0],
+            [2, 1],
+            [2, 2],
+        ],
+    ];
 
-	initGame = () => {
-		this.gridMap.forEach((line, y) => {
-			line.forEach((cell, x) => {
-				this.getCell(x, y).onclick = () => {
-					this.doPlayHuman(x, y);
-				};
-			});
-		});
+    constructor(firstPlayer = "J1") {
+        this.humanPlayer = firstPlayer;
+        this.iaPlayer = firstPlayer === "J1" ? "J2" : "J1";
+        this.initGame();
+    }
 
-		if (this.iaPlayer === 'J1') {
-			this.doPlayIa();
-		}
-	}
+    initGame = () => {
+        this.gridMap.forEach((line, y) => {
+            line.forEach((cell, x) => {
+                this.getCell(x, y).onclick = () => {
+                    this.doPlayHuman(x, y);
+                };
+            });
+        });
 
-	getCell = (x, y) => {
-		const column = x + 1;
-		const lines = ['A', 'B', 'C'];
-		const cellId = `${lines[y]}${column}`;
-		return document.getElementById(cellId);
-	}
+        if (this.iaPlayer === "J1") {
+            this.doPlayIa();
+        }
+    };
+
+    getCell = (x, y) => {
+        const column = x + 1;
+        const lines = ["A", "B", "C"];
+        const cellId = `${lines[y]}${column}`;
+        return document.getElementById(cellId);
+    };
 
     getBoardWinner = (board) => {
-        const isWinningRow = ([a, b, c]) => (
-            a !== null && a === b && b === c
-        );
+        const isWinningRow = ([a, b, c]) => a !== null && a === b && b === c;
 
         let winner = null;
 
@@ -69,22 +86,22 @@ class Morpion {
             return board[1][1];
         }
 
-        const isFull = board.every((line) => (
-			line.every((cell) => cell !== null)
-		));
-        return isFull ? 'tie' : null;
-    }
+        const isFull = board.every((line) =>
+            line.every((cell) => cell !== null)
+        );
+        return isFull ? "tie" : null;
+    };
 
-	checkWinner = (lastPlayer) => {
+    checkWinner = (lastPlayer) => {
         const winner = this.getBoardWinner(this.gridMap);
         if (!winner) {
             return;
         }
 
         this.gameOver = true;
-        switch(winner) {
-            case 'tie':
-			    this.displayEndMessage("Vous êtes à égalité !");
+        switch (winner) {
+            case "tie":
+                this.displayEndMessage("Vous êtes à égalité !");
                 break;
             case this.iaPlayer:
                 this.displayEndMessage("L'IA a gagné !");
@@ -93,44 +110,137 @@ class Morpion {
                 this.displayEndMessage("Tu as battu l'IA !");
                 break;
         }
-	}
+    };
 
-	displayEndMessage = (message) => {
-		const endMessageElement = document.getElementById('end-message');
-		endMessageElement.textContent = message;
-		endMessageElement.style.display = 'block';
-	}
+    displayEndMessage = (message) => {
+        const endMessageElement = document.getElementById("end-message");
+        endMessageElement.textContent = message;
+        endMessageElement.style.display = "block";
+    };
 
-	drawHit = (x, y, player) => {
-		if (this.gridMap[y][x] !== null) {
-			return false;
-		}
+    drawHit = (x, y, player) => {
+        if (this.gridMap[y][x] !== null) {
+            return false;
+        }
 
-		this.gridMap[y][x] = player;
+        this.gridMap[y][x] = player;
         this.turn += 1;
-		this.getCell(x, y).classList.add(`filled-${player}`);
-		this.checkWinner(player);
-		return true;
-	}
+        this.getCell(x, y).classList.add(`filled-${player}`);
+        this.checkWinner(player);
+        return true;
+    };
 
-	doPlayHuman = (x, y) => {
-		if (this.gameOver) {
-			return;
-		}
+    doPlayHuman = (x, y) => {
+        if (this.gameOver) {
+            return;
+        }
 
-		if (this.drawHit(x, y, this.humanPlayer)) {
-			this.doPlayIa();
-		}
-	}
+        // Peut-on utiliser un objet de fonctions plutôt que des if ?
+        if (this.drawHit(x, y, this.humanPlayer)) {
+            if (this.iaLevel == 1) {
+                this.doPlayIa1();
+            } else if (this.iaLevel == 2) {
+                this.doPlayIa2(this.gridMap);
+            } else if (this.iaLevel == 3) {
+                this.doPlayIa3();
+            }
+        }
+    };
 
-	doPlayIa = () => {
-		if (this.gameOver) {
-			return;
-		}
+    doPlayIa1 = () => {
+        let startTurn = this.turn;
+        while (startTurn === this.turn) {
+            let x = Math.floor(Math.random() * 3);
+            let y = Math.floor(Math.random() * 3);
+            this.drawHit(x, y, this.iaPlayer);
+        }
+    };
 
-        const { x, y } = this.minmax(this.gridMap, 0, -Infinity, Infinity, true);
+    doPlayIa2 = (board) => {
+        let startTurn = this.turn;
+
+        const isMissingOne = (row) => {
+            return (
+                row.filter((cell) => board[cell[0]][cell[1]] == this.iaPlayer)
+                    .length == 2 &&
+                row.filter((cell) => board[cell[0]][cell[1]] == null).length ==
+                    1
+            );
+        };
+
+        const findMissingOne = (row) => {
+            if (startTurn === this.turn)
+                return row.find((cell) => board[cell[0]][cell[1]] == null);
+        };
+
+        const finishTheJob = (row) => {
+            if (isMissingOne(row)) {
+                let emptyCell = findMissingOne(row);
+                this.drawHit(emptyCell[1], emptyCell[0], this.iaPlayer);
+            }
+        };
+
+        // l'IA joue en ligne
+        this.coordinates.forEach((line) => finishTheJob(line));
+
+        // l'IA joue en colonne
+        [0, 1, 2].forEach((y) => {
+            let col = [
+                [0, y],
+                [1, y],
+                [2, y],
+            ];
+            finishTheJob(col);
+        });
+
+        // l'IA joue en diagonale
+        let diagonals = [
+            [
+                [0, 0],
+                [1, 1],
+                [2, 2],
+            ],
+            [
+                [0, 2],
+                [1, 1],
+                [2, 0],
+            ],
+        ];
+
+        diagonals.forEach((diag) => finishTheJob(diag));
+
+        if (startTurn === this.turn) {
+            [0, 1, 2].forEach((y) => {
+                let col = [
+                    [0, y],
+                    [1, y],
+                    [2, y],
+                ];
+                finishTheJob(col);
+            });
+        }
+
+        while (startTurn === this.turn) {
+            let x = Math.floor(Math.random() * 3);
+            let y = Math.floor(Math.random() * 3);
+            this.drawHit(x, y, this.iaPlayer);
+        }
+    };
+
+    doPlayIa3 = () => {
+        if (this.gameOver) {
+            return;
+        }
+
+        const { x, y } = this.minmax(
+            this.gridMap,
+            0,
+            -Infinity,
+            Infinity,
+            true
+        );
         this.drawHit(x, y, this.iaPlayer);
-	}
+    };
 
     minmax = (board, depth, alpha, beta, isMaximizing) => {
         // Return a score when there is a winner
@@ -141,7 +251,7 @@ class Morpion {
         if (winner === this.humanPlayer) {
             return depth - 10;
         }
-        if (winner === 'tie' && this.turn === 9) {
+        if (winner === "tie" && this.turn === 9) {
             return 0;
         }
 
@@ -192,7 +302,7 @@ class Morpion {
                 }
             }
 
-            return (depth === 0) ? optimalMove : bestIaScore;
+            return depth === 0 ? optimalMove : bestIaScore;
         }
 
         // The lower is the score, the better is the move for the player.
@@ -214,7 +324,6 @@ class Morpion {
                 }
             }
         }
-
         return bestHumanScore;
-    }
+    };
 }
